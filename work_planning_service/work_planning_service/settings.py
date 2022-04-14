a1666c131fdd6939922c1a6d02da97ef31878d38
Nimesh Kumar <nimesh.kumar031@gmail.com>
2022-04-15 04:29:18 +0530

    Update settings.py

--- work_planning_service/work_planning_service/settings.py
+++ work_planning_service/work_planning_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$)n=x%&j)156(=k-4mex(2ypn)1fw13qlihmo6ib2c$@u-8x9-"
+SECRET_KEY = "<SECRET_KEY>"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
